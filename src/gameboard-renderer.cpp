a920f20a2937d0a08dbcd9e152eb278e3fd76bce
iluvgirlswithglasses <luunamdat204@gmail.com>
2023-03-21 10:41:25 +0700

    break here

--- src/gameboard-renderer.cpp
+++ src/gameboard-renderer.cpp
@@ -14,3 +14,33 @@ BTW I use Arch
 */
 
 #include "gameboard-renderer.h"
+
+GameboardRenderer::GameboardRenderer(uint8_t** _map, char** _scr, uint8_t _row, uint8_t _col, uint8_t _top, uint8_t _lft) {
+	map = _map;
+	scr = _scr;
+	row = _row;
+	col = _col;
+	top = _top;
+	lft = _lft;
+}
+
+void GameboardRenderer::burn() {
+	// draw empty cells first
+	for (int y = 0; y < row; y++)
+		for (int x = 0; x < col; x++)
+			if (map[y][x] == 0) draw_empty_cell(y, x);
+	// then the tiles later
+	for (int y = 0; y < row; y++)
+		for (int x = 0; x < col; x++)
+			if (map[y][x] > 0) draw_cell(y, x);
+}
+
+void GameboardRenderer::draw_cell(uint8_t y, uint8_t x) {
+
+}
+
+void GameboardRenderer::draw_empty_cell(uint8_t y, uint8_t x) {
+	// insert background image here
+	// just memcpy the background instead of memset
+	
+}
--- src/gameboard-renderer.h
+++ src/gameboard-renderer.h
@@ -17,13 +17,38 @@ BTW I use Arch
 #define GAMEBOARD_RENDERER_H
 // ------------------------------------------------------------
 
+/*
+@ about this:
+	draw the gameboard into the screen grid
+
+	note that this object does not care about the screen size
+	so beware of indexing
+*/
+
+#include <cstdint>
 #include <cstdio>
 #include "color.h"
 #include "gameboard.h"
 #include "renderer.h"
 
-struct GameBoardRenderer {
-	
+struct GameboardRenderer {
+public:
+	uint8_t** map;			// the gameboard
+	char**    scr;			// the screen
+	uint8_t   row, col;		// the size of the gameboard
+	uint8_t   top, lft;		// where the gameboard is projected on the screen
+
+	GameboardRenderer(uint8_t** map, char** scr, uint8_t row, uint8_t col, uint8_t top, uint8_t lft);
+	void burn();			// burns the gameboard into the screen grid
+
+private:
+	static constexpr uint8_t CellSize = 7;	// size of a cell on screen grid
+
+	void draw_cell(uint8_t y, uint8_t x);	// draws tile map[y, x]
+	void draw_empty_cell(uint8_t y, uint8_t x);
+
+	uint8_t get_ry(uint8_t y) { return top + CellSize * y };
+	uint8_t get_rx(uint8_t x) { return lft + CellSize * x };
 };
 
 #endif	// GAMEBOARD_RENDERER_H
